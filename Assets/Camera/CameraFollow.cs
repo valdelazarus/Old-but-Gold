5ced6d668b4b8d4a21a61622bc29966d4fc7565a
Ha Nguyen <valdelazarus@hotmail.com>
2019-09-19 19:36:19 -0400

    Implemented player's basic movement and camera follow.

---
+++ Assets/Camera/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float smooth;
+
+    Transform target;
+
+    Vector3 offset;
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        offset = transform.position - target.position;
+    }
+
+    
+    void Update()
+    {
+
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, target.position.x + offset.x, smooth * Time.deltaTime),
+            Mathf.Lerp(transform.position.y, target.position.y + offset.y, smooth * Time.deltaTime),
+            transform.position.z);
+    }
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    private const float initialYRotation = 180f;
+    private const float rotateAmount = 90f;
+    public float speed;
+    public float jumpForce;
+    public float gravityModifier;
+
+    Rigidbody rb;
+    Animator anim;
+    float actualSpeed;
+    float movement;
+
+    bool isJumping;
+    bool isOnGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    
+    void Update()
+    {
+        ProcessHorizontalMovement();
+        RotateTowardsWalkingDirection();
+        ProcessJumping();
+        UpdateAnim();
+    }
+
+    private void ProcessHorizontalMovement()
+    {
+        movement = CrossPlatformInputManager.GetAxis("Horizontal");
+        actualSpeed = movement * speed;
+        rb.velocity = Vector3.right * actualSpeed;
+    }
+
+    void RotateTowardsWalkingDirection()
+    {
+        transform.rotation = Quaternion.Euler(0f, initialYRotation - rotateAmount * movement, 0f);
+    }
+
+    void ProcessJumping()
+    {
+        CheckIfOnGround();
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && isOnGround && !isJumping)
+        {
+            transform.Translate(Vector3.up * jumpForce * Time.deltaTime);
+            isJumping = true;
+            isOnGround = false;
+        }
+        else
+        {
+            isJumping = false;
+        }
+  
+    }
+
+    
+
+    void CheckIfOnGround()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        float maxD = .1f;
+
+        if (Physics.Raycast(ray, out hit, maxD))
+        {
+            if (hit.collider != null)
+            {
+                isOnGround = true;
+            }
+            else {
+                isOnGround = false;
+            }
+        }
+    }
+
+    void UpdateAnim()
+    {
+        anim.SetFloat("Speed_f", Mathf.Abs(actualSpeed));
+        anim.SetBool("Jump_b", isJumping);
+    }
+}
