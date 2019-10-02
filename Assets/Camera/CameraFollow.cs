c5efbb2d0259b134d1769ee9ede375b336f25e98
Ha Nguyen <valdelazarus@hotmail.com>
2019-10-02 16:44:02 -0400

    Implemented UI scenes and fade-in-out transition between scenes. If you're working on Game scene, there is a "FadePanel" under HUD, if you set it active, there will be a fade-in before Game scene loads. I suggest disabling it when working on the Game scene to see what's in the scene easier.

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -21,7 +21,7 @@ public class CameraFollow : MonoBehaviour
 
         transform.position = new Vector3(
             Mathf.Lerp(transform.position.x, target.position.x + offset.x, smooth * Time.deltaTime),
-            Mathf.Lerp(transform.position.y, target.position.y + offset.y, smooth * Time.deltaTime),
+            transform.position.y,
             transform.position.z);
     }
 }
---
+++ Assets/Scenes/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    string sceneToLoad;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+
+    public void LoadScene(string sceneName)
+    {
+        sceneToLoad = sceneName;
+        anim.SetTrigger("loadScene");
+    }
+    public void LoadSelectedScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
