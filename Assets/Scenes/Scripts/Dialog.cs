572432b8a194a48dcecf8c55475f91268b672bac
FlubNub <46719188+FlubNub@users.noreply.github.com>
2019-10-27 15:37:34 -0400

    Added dialogue class and triggers

---
+++ Assets/Scenes/Scripts/Dialog.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    public Text textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject dialogPanel;
+    public GameObject continueButton;
+
+    public void AddSentences(string[] sentences)
+    {
+        Reset();
+        this.sentences = sentences;
+        dialogPanel.SetActive(true);
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textDisplay.text == sentences[index])
+            continueButton.SetActive(true);
+    }
+
+    void Reset()
+    {
+        this.sentences = null;
+        textDisplay.text = "";
+        index = 0;
+        continueButton.SetActive(false);
+        dialogPanel.SetActive(false);
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+            Reset();
+    }
+}
---
+++ Assets/Scenes/Scripts/DialogTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogTrigger : MonoBehaviour
+{
+    private GameObject dialogManager;
+    public string[] sentences;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("DialogManager").GetComponent<Dialog>().AddSentences(sentences);
+    }
+}
