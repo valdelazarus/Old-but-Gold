38c0e196b08f467fa2e80840e713e8904fa7905d
FlubNub <46719188+FlubNub@users.noreply.github.com>
2019-10-29 19:53:39 -0400

    Bug Fixes

--- Assets/Scenes/Collectibles/Scripts/HealthCapsuleMovement.cs
+++ Assets/Scenes/Collectibles/Scripts/HealthCapsuleMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class HealthCapsuleMovement : MonoBehaviour
 {
     private Vector3 startPosition;
     bool up = true;
