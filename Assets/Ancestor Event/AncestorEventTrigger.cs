3cb31fd969ab3efee498b2eafc23515556590bf2
Ha Nguyen <valdelazarus@hotmail.com>
2019-11-08 18:51:19 -0500

    Implemented ancestor ghost event and organize assets into proper folders
    Create new folder named Ancestor Event with event related scripts and prefab. populateEnemies.cs moved to Enemy Spawner folder. Reset.cs and SetLayerWeight.cs moved to General State Machine Behavior folder. NPC_Ghost Animator controller moved to Ghost/Animation folder. Modified GhostNPC prefab to implement event behavior.

---
+++ Assets/Ancestor Event/AncestorEventTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AncestorEventTrigger : MonoBehaviour
+{
+    public string[] sentences;
+
+    AncestorDialog dialog;
+    Animator ghostAnim;
+    void Start()
+    {
+        ghostAnim = GetComponentInChildren<Animator>();
+        dialog = GetComponentInChildren<AncestorDialog>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //load sentences into ghost
+            dialog.SetDialogueSentences(sentences);
+
+            //enable ancestor ghost with its appearance animation 
+            ghostAnim.SetTrigger("Appear");
+
+            // end of animation, activate dialog - in AncestorDialog script
+
+            //Destroy(gameObject,1f); // destroy this trigger once activated
+        }
+    }
+
+
+}
