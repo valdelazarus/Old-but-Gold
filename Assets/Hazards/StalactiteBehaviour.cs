9e2c25eeec9f05879264bbd3c0ebc92249cf8e55
indominusLeo <35774415+indominusLeo@users.noreply.github.com>
2019-11-19 11:37:49 -0500

    Added Stalactite Hazard
    - When the player is below the stalactite, it will fall and hurt the player.
    - They can be found on the cave's ceiling.
    - NEED TO: Add both new hazards to the level design, and implement their respective sound effects (Explosion for the bomb, falling sound for the stalactite).

---
+++ Assets/Hazards/StalactiteBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactiteBehaviour : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fall()
+    {
+        rb.isKinematic = false;
+        GetComponent<BoxCollider>().enabled = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            HUDManager.RemoveHealth(0.1f);
+        }
+        Destroy(gameObject);
+    }
+}
